ad713278df54920f0f51007cad42b876e0235090
Abhisha Shah <abhisha5993@gmail.com>
2021-11-01 10:15:16 -0400

    Implemented react router

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
+import { Card } from 'react-bootstrap'
 import Rating from './Rating';
 
 const Product = ({_id,name,image,price,rating,numReviews}) => {
     return (
             <Card className="my-3 p-3 rounded">
-                <a href={`/product/${_id}`}> 
+                <Link to={`/product/${_id}`}> 
                     <Card.Img variant="top" src={image} />
-                </a>
+                </Link>
                 <Card.Body>
-                     <a href={`/product/${_id}`}> 
+                     <Link to={`/product/${_id}`}> 
                         <Card.Title as="div"> <strong>{name}</strong></Card.Title>
-                    </a>  
+                    </Link>  
                     <Card.Text as="div">
                         <div className="my-3">
                             <Rating value={rating} reviews={numReviews}/>
