faac267c85e575ad962175200d7b4a9aae96be67
Abhisha Shah <abhisha5993@gmail.com>
2021-11-02 13:19:34 -0400

    Implmented esmodules syntax

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require("express")
-const dotenv = require("dotenv")
-const products = require("./data/productsData")
+import express from "express"
+import dotenv from "dotenv"
+import products from "./data/productsData.js" // for js file need to add .js extension for es module syntax
 
 dotenv.config()
 
