b6e42e8859ac6d50ce8ae95fd5ac0440ad71bca6
Abhisha Shah <abhisha5993@gmail.com>
2021-11-02 22:02:47 -0400

    connected to mongo db using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express"
 import dotenv from "dotenv"
-import products from "./data/productsData.js" // for js file need to add .js extension for es module syntax
+import connectDB from "./config/db.js"
+import products from "./data/productsData.js" // for js file need to add .js extension for es module with Node
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
