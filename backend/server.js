5b3a3dd7b5bcd64f4401e7607300722d675852b3
Abhisha Shah <abhisha5993@gmail.com>
2021-11-05 17:18:57 -0400

    created routes and get product data from database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
-import connectDB from "./config/db.js"
-import products from "./data/productsData.js" // for js file need to add .js extension for es module with Node
+import connectDB from "./config/db.js"  // for js file need to add .js extension for es module with Node
 
+import productRoutes from "./routes/productRoutes.js"
 dotenv.config()
 
 connectDB()
@@ -11,18 +11,11 @@ const app = express()
 
 // if request made on “/” - root, send simple message
 app.get('/',function(req,res){
-    res.send('API is running....'); // send as response
+    res.send('API is running....') // send as response
    })
 
-// get all products data
-app.get('/api/products',function(req,res){
-    res.json(products);
-})
-// get product details based in id
-app.get('/api/products/:id',function(req,res){
-    const product = products.find(p => p._id === req.params.id)
-    res.send(product);
-})
+app.use('/api/products',productRoutes)
+
 const PORT = process.env.PORT || 5000
    
 app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
