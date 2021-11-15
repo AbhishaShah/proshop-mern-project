b4c505a94be6b0e4fceddca29b2e02b64693ea58
Abhisha Shah <abhisha5993@gmail.com>
2021-11-15 10:13:25 -0500

    Added user registartion and pasword hashing

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -27,9 +27,19 @@ const userSchema = mongoose.Schema(
   }
 )
 
+// compare login screen entered password with db stored encrypted password
 userSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
+
+// encrypt password before saving to db
+userSchema.pre('save',async function (next) {
+  if(!this.isModified('password')){
+    next()
+  }
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
 const User = mongoose.model('User', userSchema)
 
