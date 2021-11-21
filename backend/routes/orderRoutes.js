a1e897be2cb76d9a08fe0669cd682a52bff4b152
Abhisha Shah <abhisha5993@gmail.com>
2021-11-21 16:46:47 -0500

    Created get order details action,reducer and controller

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 const router = express.Router()
-import { addOrderItems} from '../controllers/OrderControllers.js'
+import { addOrderItems, getOrderByID} from '../controllers/OrderControllers.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItems)
+router.route('/:id').post(protect, getOrderByID)
 
 export default router   
