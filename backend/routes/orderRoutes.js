c6ed4ecb63b0493ca43169ede0ce672cc0ba4fa6
Abhisha Shah <abhisha5993@gmail.com>
2021-11-25 20:00:23 -0500

    Created controller and route to get user specific orders

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import { addOrderItems, getOrderByID, updateOrderToPaid} from '../controllers/OrderControllers.js'
+import { addOrderItems, getOrderByID, updateOrderToPaid, getMyOrders} from '../controllers/OrderControllers.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItems)
 router.route('/:id').get(protect, getOrderByID)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
+router.route('/myorders').get(protect, getMyOrders)
 
 export default router   
